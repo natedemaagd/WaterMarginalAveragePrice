15b7aa48b0ef79fdf0914825d9dc2cb2b40f0653
Nathan DeMaagd <nate.demaagd@gmail.com>
2020-09-27 13:57:43 -1000

    change distribution plot from density to boxplot, and add present-day water use

--- MAIN/Code/Analysis/02-optimal-consumption.R
+++ MAIN/Code/Analysis/02-optimal-consumption.R
@@ -46,27 +46,37 @@ all.sfds$avg.30day.use_optimal_MUC426 <- optimal_consumption(data = all.sfds, A 
 
 # melt data
 water_use_melt <- data.frame(value    = c(all.sfds$avg.30day.use, all.sfds$avg.30day.use_optimal, all.sfds$avg.30day.use_optimal_MUC213, all.sfds$avg.30day.use_optimal_MUC426),
-                             variable = rep(factor(c('Current use', 'Utility maximizing', 'Socially optimal, MUC = $2.13/tgal', 'Socially optimal, MUC = $4.26/tgal'),
-                                                   levels = c('Current use', 'Utility maximizing', 'Socially optimal, MUC = $2.13/tgal', 'Socially optimal, MUC = $4.26/tgal')),
+                             variable = rep(factor(c('Current use', 'Present-day\nutility maximizing', 'Socially optimal,\nMUC = $2.13/tgal', 'Socially optimal,\nMUC = $4.26/tgal'),
+                                                   levels = c('Current use', 'Present-day\nutility maximizing', 'Socially optimal,\nMUC = $2.13/tgal', 'Socially optimal,\nMUC = $4.26/tgal')),
                                             each = nrow(all.sfds)))
 
 
 # plot distributions of water use: current, 
 ggplot(data = water_use_melt) + geom_boxplot(aes(x = variable, y = value/1000),
                                              outlier.shape = NA) +
+  
+  # change axis labels
   labs(x = 'Consumption type', y = 'Household consumption (tgal/month)') +
-  theme(legend.title = element_blank(), text = element_text(size = 15)) +
+  
+  # adjust plot appearance
+  theme(legend.title = element_blank(), text = element_text(size = 15),
+        axis.ticks = element_blank(),
+        panel.grid.major.x = element_blank()) +
+  
+  # adjust y scale to omit outliers
   coord_cartesian(ylim = quantile(water_use_melt$value/1000, c(0, 0.95), na.rm = TRUE)) +
+  
+  # add median use to median bar in boxplots
   annotate("text", x = 1:4,
            y     = c(      median(water_use_melt[water_use_melt$variable == 'Current use',                        1], na.rm = TRUE)/1000+0.5,
-                           median(water_use_melt[water_use_melt$variable == 'Utility maximizing',                 1], na.rm = TRUE)/1000+0.5,
-                           median(water_use_melt[water_use_melt$variable == 'Socially optimal, MUC = $2.13/tgal', 1], na.rm = TRUE)/1000+0.5,
-                           median(water_use_melt[water_use_melt$variable == 'Socially optimal, MUC = $4.26/tgal', 1], na.rm = TRUE)/1000+0.5),
+                           median(water_use_melt[water_use_melt$variable == 'Present-day\nutility maximizing',                 1], na.rm = TRUE)/1000+0.5,
+                           median(water_use_melt[water_use_melt$variable == 'Socially optimal,\nMUC = $2.13/tgal', 1], na.rm = TRUE)/1000+0.5,
+                           median(water_use_melt[water_use_melt$variable == 'Socially optimal,\nMUC = $4.26/tgal', 1], na.rm = TRUE)/1000+0.5),
            
            label = c(format(round(median(water_use_melt[water_use_melt$variable == 'Current use',                        1], na.rm = TRUE)/1000, 2), nsmall = 2),
-                     format(round(median(water_use_melt[water_use_melt$variable == 'Utility maximizing',                 1], na.rm = TRUE)/1000, 2), nsmall = 2),
-                     format(round(median(water_use_melt[water_use_melt$variable == 'Socially optimal, MUC = $2.13/tgal', 1], na.rm = TRUE)/1000, 2), nsmall = 2),
-                     format(round(median(water_use_melt[water_use_melt$variable == 'Socially optimal, MUC = $4.26/tgal', 1], na.rm = TRUE)/1000, 2), nsmall = 2)))
+                     format(round(median(water_use_melt[water_use_melt$variable == 'Present-day\nutility maximizing',                 1], na.rm = TRUE)/1000, 2), nsmall = 2),
+                     format(round(median(water_use_melt[water_use_melt$variable == 'Socially optimal,\nMUC = $2.13/tgal', 1], na.rm = TRUE)/1000, 2), nsmall = 2),
+                     format(round(median(water_use_melt[water_use_melt$variable == 'Socially optimal,\nMUC = $4.26/tgal', 1], na.rm = TRUE)/1000, 2), nsmall = 2)))
 
-ggsave(filename = 'MAIN/Figures/Images/02-optimal-water-use-distributions.png', height = 5, width = 5, dpi = 300)
+ggsave(filename = 'MAIN/Figures/Images/02-optimal-water-use-distributions.png', height = 5, width = 7, dpi = 300)
 
