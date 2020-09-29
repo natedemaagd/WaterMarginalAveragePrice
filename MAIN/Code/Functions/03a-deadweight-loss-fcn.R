88818e9a82d19d05fb08693aa49b13df41a96727
Nathan DeMaagd <nate.demaagd@gmail.com>
2020-09-28 17:31:07 -1000

    finish DWL function, begin DWL analysis script

--- MAIN/Code/Functions/03a-deadweight-loss-fcn.R
+++ MAIN/Code/Functions/03a-deadweight-loss-fcn.R
@@ -1,6 +1,20 @@
 
 
 
+# example data
+q_actual               <- 14128.36
+q_optimal              <- 14440
+A                      <- 2.112406e+67
+no.sewer               <- 'Sewer'
+elasticity_demand      <- -0.06
+volume_sewer_rate      <- 0.00463
+volume_water_blk1_qty  <- 13000
+volume_water_blk2_qty  <- 30000
+volume_water_blk1_rate <- 0.00442
+volume_water_blk2_rate <- 0.00533
+volume_water_blk3_rate <- 0.00749
+
+
 
 # given the actual consumption and optimal consumption of the household, calculate its deadweight loss ($/month)
 
@@ -14,12 +28,12 @@ deadweight_loss <- function(q_actual, q_optimal, A, no.sewer = 'Other'){
                       NA,
                       integrate(f     = function(x){ A*x^(1/elasticity_demand) },
                                 lower = q_actual,
-                                upper = q_optimal))
+                                upper = q_optimal))[[1]]
   
   
   
   # HH water bill - actual and optimal BOTH in FIRST block
-  if(q_actual >= 0 & q_actual < volume_water_blk1_qty & q_optimal >=0 & q_optimal < volume_water_blk1_qty){
+  if(q_actual >= 0 & q_actual < volume_water_blk1_qty & q_optimal >=0 & q_optimal < volume_water_blk1_qty & !is.na(q_actual) & !is.na(q_optimal)){
     
     # get integral from water bill
     bottom_curve <- abs(q_optimal - q_actual) * volume_water_blk1_rate
@@ -28,15 +42,14 @@ deadweight_loss <- function(q_actual, q_optimal, A, no.sewer = 'Other'){
     hh_dwl <- ifelse(no.sewer == 'Other',
                      top_curve - bottom_curve,
                      ifelse(no.sewer == 'Sewer',
-                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate)),
-                     NA)
+                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate), NA))
     
-    return(hh_dwl)
+    hh_dwl
   
     
     
   # HH water bill - actual and optimal BOTH in SECOND block    
-  } else if(q_actual >= volume_water_blk2_qty & q_actual < volume_water_blk3_qty & q_optimal >= volume_water_blk2_qty & q_optimal < volume_water_blk3_qty) {
+  } else if(q_actual >= volume_water_blk1_qty & q_actual < volume_water_blk2_qty & q_optimal >= volume_water_blk1_qty & q_optimal < volume_water_blk2_qty & !is.na(q_actual) & !is.na(q_optimal)) {
     
     # get integral from water bill
     bottom_curve <- abs(q_optimal - q_actual) * volume_water_blk2_rate
@@ -45,15 +58,14 @@ deadweight_loss <- function(q_actual, q_optimal, A, no.sewer = 'Other'){
     hh_dwl <- ifelse(no.sewer == 'Other',
                      top_curve - bottom_curve,
                      ifelse(no.sewer == 'Sewer',
-                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate)),
-                     NA)
+                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate), NA))
     
-    return(hh_dwl)
+    hh_dwl
    
   
     
   # HH water bill - actual and optimal BOTH in THIRD block   
-  } else if(q_actual >= volume_water_blk3_qty & q_optimal >= volume_water_blk3_qty) {
+  } else if(q_actual >= volume_water_blk2_qty & q_optimal >= volume_water_blk2_qty & !is.na(q_actual) & !is.na(q_optimal)) {
     
     # get integral from water bill
     bottom_curve <- abs(q_optimal - q_actual) * volume_water_blk3_rate
@@ -62,15 +74,14 @@ deadweight_loss <- function(q_actual, q_optimal, A, no.sewer = 'Other'){
     hh_dwl <- ifelse(no.sewer == 'Other',
                      top_curve - bottom_curve,
                      ifelse(no.sewer == 'Sewer',
-                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate)),
-                     NA)
+                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate), NA))
     
-    return(hh_dwl)
+    hh_dwl
     
     
     
   # HH water bill - actual in FIRST, optimal in SECOND  
-  } else if(q_actual < volume_water_blk1_qty & q_optimal >= volume_water_blk2_qty & q_optimal) {
+  } else if(q_actual < volume_water_blk1_qty & q_optimal >= volume_water_blk1_qty & q_optimal < volume_water_blk2_qty & !is.na(q_actual) & !is.na(q_optimal)) {
     
     # get integral from water bill
     bottom_curve <- (abs(volume_water_blk1_qty - q_actual) * volume_water_blk1_rate) + (abs(q_optimal - volume_water_blk1_qty) * volume_water_blk2_rate)
@@ -79,15 +90,14 @@ deadweight_loss <- function(q_actual, q_optimal, A, no.sewer = 'Other'){
     hh_dwl <- ifelse(no.sewer == 'Other',
                      top_curve - bottom_curve,
                      ifelse(no.sewer == 'Sewer',
-                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate)),
-                     NA)
+                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate), NA))
     
-    return(hh_dwl)
+    hh_dwl
     
   
     
   # HH water bill - actual in SECOND, optimal in THIRD  
-  } else if(q_actual < volume_water_blk2_qty & q_optimal >= volume_water_blk2_qty) {
+  } else if(q_actual >= volume_water_blk1_qty & q_actual < volume_water_blk2_qty & q_optimal >= volume_water_blk2_qty & !is.na(q_actual) & !is.na(q_optimal)) {
     
     # get integral from water bill
     bottom_curve <- (abs(volume_water_blk2_qty - q_actual) * volume_water_blk2_rate) + (abs(q_optimal - volume_water_blk3_qty) * volume_water_blk3_rate)
@@ -96,17 +106,16 @@ deadweight_loss <- function(q_actual, q_optimal, A, no.sewer = 'Other'){
     hh_dwl <- ifelse(no.sewer == 'Other',
                      top_curve - bottom_curve,
                      ifelse(no.sewer == 'Sewer',
-                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate)),
-                     NA)
+                            abs(top_curve - bottom_curve - abs(q_optimal - q_actual)*volume_sewer_rate), NA))
     
-    return(hh_dwl)
+    hh_dwl
     
     
     
   # catch-all for NA, etc.  
   } else {
     
-    return(NA)
+    NA
     
   }
   
